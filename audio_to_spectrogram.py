d73e53960138da740abbee59d8b7e14d47233f5c
lucirie <lucirieko@gmail.com>
2025-11-02 10:53:14 +0200

    trims audio before conversion into spectrogram uses AudioSegment to trim the audio and export the new wav to a tmp directory

--- audio_to_spectrogram.py
+++ audio_to_spectrogram.py
@@ -4,6 +4,7 @@ from scipy.io import wavfile
 
 # Open 'songs' folder and itterate over each .wav file
 import os
+from pydub import AudioSegment
 
 directory = os.fsencode('songs')
 
@@ -11,8 +12,15 @@ for file in os.listdir(directory):
     filename = os.fsdecode(file)
 
     if filename.endswith('.wav'):
+        # Trim first 30 seconds of audio
+        input_path = f'songs/{file}'
+        output_path = 'tmp'
+        audio = AudioSegment.from_wav(input_path)
+        trimmed_audio = audio[0:30000]
+        trimmed_audio.export(output_path, format='wav')
+
         # Convert into spectrogram
-        sample_rate, samples = wavfile.read(file)
+        sample_rate, samples = wavfile.read(f'{output_path}/{file}.wav')
         frequencies, times, spectrogram = signal.spectrogram(samples, sample_rate)
         continue
     else:
