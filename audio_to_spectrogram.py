bb0a4a7e08d66d00fb45b0e87caa68b8611cb985
lucirie <lucirieko@gmail.com>
2025-11-03 17:06:40 +0200

    Plot and download spectrogram with matplotlib

--- audio_to_spectrogram.py
+++ audio_to_spectrogram.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+import numpy as np
 from scipy import signal
 from scipy.io import wavfile
 
@@ -13,15 +14,24 @@ for file in os.listdir(directory):
 
     if filename.endswith('.wav'):
         # Trim first 30 seconds of audio
-        input_path = f'songs/{file}'
-        output_path = 'tmp'
+        input_path = f'songs/{filename}'
+        trimmed_path = f'trimmed_songs/{filename}'
         audio = AudioSegment.from_wav(input_path)
-        trimmed_audio = audio[0:30000]
-        trimmed_audio.export(output_path, format='wav')
+        trimmed_audio = audio[:30000]
+        trimmed_audio.export(trimmed_path, format='wav')
 
         # Convert into spectrogram
-        sample_rate, samples = wavfile.read(f'{output_path}/{file}.wav')
+        sample_rate, samples = wavfile.read(trimmed_path)
         frequencies, times, spectrogram = signal.spectrogram(samples, sample_rate)
+
+        # Plot spectrogram
+        plt.figure(figsize=(10, 4))
+        plt.pcolormesh(times, frequencies, 10 * np.log10(spectrogram + 1e-10), shading='gouraud')
+        plt.ylabel('Frequency [Hz]')
+        plt.xlabel('Time [sec]')
+
+        destination = 'classical' # Change catagory for training data, else set as 'input'
+        plt.savefig(f'spectrogram/{destination}/{filename}.png', dpi=300)
         continue
     else:
         print("non-wav audiofiles not supported")
